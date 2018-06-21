40011a6f765d7694cf2bf34af2a78189b6e00b0f
Carter <carter.s.morgan@gmail.com>
2018-06-21 15:44:32 +0000

    change to syntax in ticker.py

--- ticker.py
+++ ticker.py
@@ -1,31 +1,27 @@
 from flask import Flask,render_template,request,redirect
 from stockfunction import stock_plot
-application = Flask(__name__)
+ticker = Flask(__name__)
 
-application.vars={}
+ticker.vars={}
 
 
-<<<<<<< HEAD
-@application.route('/index',methods=['GET','POST'])
-=======
-@app.route('/index',methods=['GET','POST'])
->>>>>>> origin
+@ticker.route('/index',methods=['GET','POST'])
 def index():
     if request.method == 'GET':
         return render_template('userinfo.html')
     else:
         #request was a POST
-        app.vars['ticker'] = request.form['ticker']
-        app.vars['month'] = request.form['month']
+        ticker.vars['ticker'] = request.form['ticker']
+        ticker.vars['month'] = request.form['month']
 
-	stock_plot(app.vars['ticker'],app.vars['month'])
+	stock_plot(ticker.vars['ticker'],ticker.vars['month'])
 	
         return redirect('/next')
 
-@application.route('/next',methods=['GET'])
+@ticker.route('/next',methods=['GET'])
 def next():  
 
     return render_template('stock.html') 
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0')
+    ticker.run(host='0.0.0.0')
