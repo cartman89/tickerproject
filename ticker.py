cdb62af64606e7c71700a41efed9d76f1035b790
Carter <carter.s.morgan@gmail.com>
2018-06-21 15:49:33 +0000

    change syntax for ticker.py again

--- ticker.py
+++ ticker.py
@@ -1,27 +1,27 @@
 from flask import Flask,render_template,request,redirect
 from stockfunction import stock_plot
-ticker = Flask(__name__)
+app = Flask(__name__)
 
-ticker.vars={}
+app.vars={}
 
 
-@ticker.route('/index',methods=['GET','POST'])
+@app.route('/index',methods=['GET','POST'])
 def index():
     if request.method == 'GET':
         return render_template('userinfo.html')
     else:
         #request was a POST
-        ticker.vars['ticker'] = request.form['ticker']
-        ticker.vars['month'] = request.form['month']
+        app.vars['ticker'] = request.form['ticker']
+        app.vars['month'] = request.form['month']
 
-	stock_plot(ticker.vars['ticker'],ticker.vars['month'])
+	stock_plot(app.vars['ticker'],app.vars['month'])
 	
         return redirect('/next')
 
-@ticker.route('/next',methods=['GET'])
+@app.route('/next',methods=['GET'])
 def next():  
 
     return render_template('stock.html') 
 
 if __name__ == "__main__":
-    ticker.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
